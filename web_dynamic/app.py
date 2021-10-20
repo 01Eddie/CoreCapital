a2e001a45430e3c1eab469ce1389fe2722601b03
01Eddie <jesus.11.97@hotmail.com>
2021-10-20 12:14:54 -0500

    [ADD] api

--- web_dynamic/app.py
+++ web_dynamic/app.py
@@ -69,25 +69,9 @@ VALUES (1,1,'admin','admin','admin@gmail.com','11111111','999999999',1,'2021-10-
 def modal():
     return render_template('modal.html')
 
-""" @app.route("/modal", methods=['GET', 'POST'], strict_slashes=False)
-def modal():
-    return redirect(url_for('question'))
- """
-
-@app.route("/question", methods=['GET', 'POST'], strict_slashes=False)
-def question():
-    cur = mysql.connection.cursor()
-    """ 
-    INSERT INTO `Question_Options` (`id_question_option`,`id_question`,`id_survey`,`id_survey_section`,`name_option`,`value`,`active`,`order`)
-    VALUES (1,1,1,1,'1. Masculino',1,1,1),(2,1,1,1,'2. Femenino',2,1,2),
-    (3,2,1,1,'1. Sí.',3,1,3),(4,2,1,1,'2. No.',4,1,4),
-    (5,3,1,1,'1. Sí.',5,1,5),(6,3,1,1,'2. No.',6,1,6),
-    (7,4,1,2,'1. Extremadamente bajo.',1,1,1),(8,4,1,2,'2. Muy bajo.',2,1,2),(9,4,1,2,'3. Bajo.',3,1,3),(10,4,1,2,'4. Promedio.',4,1,4),(11,4,1,2,'5. Alto.',5,1,5),(12,4,1,2,'6. Muy alto.',6,1,6),(13,4,1,2,'7. Extremadamente alto.',7,1,7) 
-    """
-    cur.execute("INSERT INTO Users (id_question_option,id_question,id_survey,id_survey_section,name_option,value,active,order) VALUES (%s,%s,%s,%s,%s,%s,%s)", """ (numero_documento, name, lastname, email, nmro_document, phone, active, created_at, created_by)) """
-    mysql.connection.commit()
-    flash('Added inversors successfully')
-    return redirect(url_for('modal'))
+# @app.route("/modal", methods=['GET', 'POST'], strict_slashes=False)
+# def modal():
+#     return redirect(url_for('question'))
 
 
 @app.errorhandler(404)
