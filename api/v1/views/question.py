f8c53dc86edf408afa7294e45633c0a9c4325370
andres-condezo <ra.condezomonge@gmail.com>
2021-10-25 03:03:17 -0500

    update

--- api/v1/views/question.py
+++ api/v1/views/question.py
@@ -2,7 +2,15 @@
 """ objects that handle all default RestFul API actions for Places """
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, render_template, request
+from models.question import Question
 
-@app_views.route('/questions', strict_slashes=False)
+@app_views.route('/questions', method=['GET'], strict_slashes=False)
 def quiz():
-    return render_template('questions.html')
+    """
+    Return all questions
+    """
+    list_question = []
+    all_questions = Question.query.all()
+    for question in all_questions:
+        list_question.append(question.to_dict())
+    return jsonify(list_question)
