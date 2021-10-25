166947546a1041b6df793e5f80f6c19f6692e37f
andres-condezo <ra.condezomonge@gmail.com>
2021-10-25 13:55:19 -0500

    [add]: add render function

--- web_dynamic/static/scripts/render.js
+++ web_dynamic/static/scripts/render.js
@@ -1,24 +1,28 @@
 import { createElement, Render, appendRender } from '../scripts/libreria.js';
 
-// imgSrc="{{url_for('static', filename='scripts/render.js')}}"
-
 const url = 'http://localhost:5001/api/v1/surveys/1/sections/1/questions/1';
 
 $.get(url, function (res) {
   const nameQuestion = res.name_question;
-  $();
-
-  console.log(nameQuestion);
+  // const update = $('DIV#p_question');
+  // const question = $('p_question');
+  // $(update).click(function () {
+  // header.text('New Header!!!');
+  // $(nameQuestion).appendTo('#p_question');
+  $('text_placeholder').appendTo('#p_question');
 });
 
-const question = createElement('p', {
-  class: 'question'
-  // alt: 'Logo',
-  // src: '../images/CorePartners_LogoRGB_positivo.png'
-});
+// console.log(nameQuestion);
+// });
+
+// const question = createElement('p', {
+//   class: 'question'
+// alt: 'Logo',
+// src: '../images/CorePartners_LogoRGB_positivo.png'
+// });
 
 // const Nav = createElement('nav', { class: 'main-nav' });
 
 // const Header = createElement('header', { class: 'main-header' }, [Logo, Nav]);
 
-Render(question, document.getElementById('question'));
+// Render(question, document.getElementById('question'));
