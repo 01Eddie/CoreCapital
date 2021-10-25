1d9d7debb14bdc846e2c52724e70b49281bebe94
andres-condezo <ra.condezomonge@gmail.com>
2021-10-25 15:39:08 -0500

    [add]: add query request to render function

--- web_dynamic/static/scripts/render.js
+++ web_dynamic/static/scripts/render.js
@@ -1,28 +1,8 @@
-import { createElement, Render, appendRender } from '../scripts/libreria.js';
+// import { createElement, Render, appendRender } from '../scripts/libreria.js';
 
-const url = 'http://localhost:5001/api/v1/surveys/1/sections/1/questions/1';
+const url = 'http://localhost:5001/api/v1/surveys/1/sections/1/questions/2';
 
 $.get(url, function (res) {
   const nameQuestion = res.name_question;
-  // const update = $('DIV#p_question');
-  // const question = $('p_question');
-  // $(update).click(function () {
-  // header.text('New Header!!!');
-  // $(nameQuestion).appendTo('#p_question');
-  $('text_placeholder').appendTo('#p_question');
+  $('#p_question').text(nameQuestion);
 });
-
-// console.log(nameQuestion);
-// });
-
-// const question = createElement('p', {
-//   class: 'question'
-// alt: 'Logo',
-// src: '../images/CorePartners_LogoRGB_positivo.png'
-// });
-
-// const Nav = createElement('nav', { class: 'main-nav' });
-
-// const Header = createElement('header', { class: 'main-header' }, [Logo, Nav]);
-
-// Render(question, document.getElementById('question'));
