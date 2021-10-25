385ea6d6518d553010c9932856c8064f93774308
01Eddie <jesus.11.97@hotmail.com>
2021-10-25 02:01:54 -0500

    [ADD] set_db

--- models/question_option.py
+++ models/question_option.py
@@ -16,7 +16,7 @@ class Question_Option(BaseModel, Base):
     """This class defines a question_option by various attributes"""
     __tablename__ = 'Question_Options'
 
-    id_question = Column(Integer, ForeignKey('Question.id'))
+    id_question = Column(Integer, ForeignKey(Question.id))
     id_survey = Column(Integer, nullable=False)
     id_survey_section = Column(Integer, nullable=False)
     name_option = Column(String(200), nullable=False)
