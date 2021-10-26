19b1b67360bc9d7c81fd0fe7e0a95186ad885514
andres-condezo <ra.condezomonge@gmail.com>
2021-10-25 23:21:22 -0500

    [add]: render question

--- web_dynamic/static/scripts/render.js
+++ web_dynamic/static/scripts/render.js
@@ -1,8 +1,13 @@
 // import { createElement, Render, appendRender } from '../scripts/libreria.js';
 
-const url = 'http://localhost:5001/api/v1/surveys/1/sections/1/questions/2';
+const url = 'http://localhost:5001/api/v1/surveys/1/sections/2/questions/3';
 
 $.get(url, function (res) {
   const nameQuestion = res.name_question;
+  const options = res.answer_options;
   $('#p_question').text(nameQuestion);
+  $('#text-buttons').html('');
+  options.forEach((el) => {
+    $('#text-buttons').append(`<button type="button" class="botones btn-light">${el.name_option}</button>`);
+  });
 });
