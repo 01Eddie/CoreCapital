b6a05f390c5819f98cc73c8ab58c9515a2e54188
andres-condezo <ra.condezomonge@gmail.com>
2021-10-26 13:57:46 -0500

    [add] add script to render all questions

--- api/v1/views/question.py
+++ api/v1/views/question.py
@@ -4,6 +4,7 @@ from api.v1.views import app_views
 from flask import abort, jsonify, make_response, render_template, request
 from models.question import Question
 from models import session
+from models.survey_section import Survey_Section
 
 @app_views.route('/questions', methods=['GET'], strict_slashes=False)
 def all_question():
@@ -17,6 +18,8 @@ def all_question():
         # if not question.answer_options:
         #     print(question.name_question)
         questionOpt["answer_options"] = [op.to_dict() for op in question.answer_options] if question.answer_options else []
+        section = session.query(Survey_Section).filter_by(id=question.id_survey_section).first()
+        questionOpt["section_name"] = section.name_section if section is not None else ''
         # print(question.answer_options)
         list_question.append(questionOpt)
     return jsonify(list_question)
--- web_dynamic/static/scripts/render.js
+++ web_dynamic/static/scripts/render.js
@@ -1,24 +1,41 @@
 // import { createElement, Render, appendRender } from '../scripts/libreria.js';
 
-const url = 'http://localhost:5001/api/v1/surveys/1/sections/2/questions/4';
+const url = 'http://localhost:5001/api/v1/surveys/1/sections/1/questions/1';
 
 /* console.log(url); */
 const questions_url = 'http://localhost:5001/api/v1/questions';
 // console.log("API QUESTIONS");
 // console.log(questions_url);
-$.get(url, function (res) {
+
+const answers = [];
+let questions = [];
+let currentIndex = 0;
+
+function render_question (res) {
   const nameQuestion = res.name_question;
   const options = res.answer_options;
   $('#p_question').text(nameQuestion);
   $('#text-buttons').html('');
   options.forEach((el) => {
-    $('#text-buttons').append(`<button type="button" class="botones btn-light">${el.name_option}</button>`);
+    const id = el.id;
+    $('#text-buttons').append(`<button id='${id}' type="button" class="botones btn-light">${el.name_option}</button>`);
   });
-});
+  $('#text-buttons button').click(function (arg) {
+  // if (currentIndex = question.length) {
+  //   console.log('final')
+  // }
+    currentIndex++;
+
+    render_question(questions[currentIndex]);
+    i;
+  });
+}
 
 setTimeout(
   function () {
     $.get(questions_url, function (res) {
-      console.log(res);
+      console.log(res[0]);
+      questions = res;
+      render_question(res[currentIndex]);
     });
   }, 0);
