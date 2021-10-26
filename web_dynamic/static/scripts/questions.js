d5285f2ec6c7b7e8a8df736bc93a37f01c155b65
andres-condezo <ra.condezomonge@gmail.com>
2021-10-25 19:19:42 -0500

    [add]: add questions script

---
+++ web_dynamic/static/scripts/questions.js
@@ -0,0 +1,16 @@
+function myFunction () {
+  // $.ajax({
+  // 		type: 'POST',  // Envío con método POST
+  // 		url: './question_options',  // Fichero destino (el PHP que trata los datos)
+  // 		data: { nom: pnom, pass: ppass } // Datos que se envían
+  // 		}).done(function( msg ) {  // Función que se ejecuta si todo ha ido bien
+  // 			$("#consola").html(msg);  // Escribimos en el div consola el mensaje devuelto
+  // 		}).fail(function (jqXHR, textStatus, errorThrown){ // Función que se ejecuta si algo ha ido mal
+  // 		// Mostramos en consola el mensaje con el error que se ha producido
+  // 		$("#consola").html("The following error occured: "+ textStatus +" "+ errorThrown);
+  // 		});
+  console.log('click');
+}
+
+{ /* document.getElementsByClassName("botones").onclick = function() {myFunction()}; */ }
+$('.botones').click(myFunction);
