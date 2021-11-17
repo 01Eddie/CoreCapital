6b86d1e7b1c766b325a9748a36e9b6f0ee9d5ab0
andres-condezo <ra.condezomonge@gmail.com>
2021-11-17 14:03:50 -0500

    [update]: Change the style for the id_risk_profile

--- api/v1/views/answer.py
+++ api/v1/views/answer.py
@@ -3,13 +3,14 @@
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, render_template, request
 from models.question import Question
-from models import session
+import models
 from models.survey_section import Survey_Section
 from models.answer import Answer
 from models.evaluation import Evaluation
 from models.risk_profile import Risk_Profile
 from models.user import User
 
+# session = models.session
 
 # @app_views.route('/risk_profile', methods=['POST'], strict_slashes=False)
 # def save_answers():
@@ -37,45 +38,53 @@ def save_answers():
     id_age = data.get('user_age')
     for answer in answer_list:
         ans = Answer(
-        id_user = answer.get('user_id'),
-        id_question_option = answer.get('id_question_option'),
-        answer_value = answer.get('answer_value'),
-        active = answer.get('active'),
-        id_question = answer.get('id_question'),
-        id_survey = answer.get('id_survey'),
-        id_survey_section = answer.get('id_survey_section'),
-        create_by = answer.get('user_id')
+        id_user=answer.get('user_id'),
+        id_question_option=answer.get('id_question_option'),
+        answer_value=answer.get('answer_value'),
+        active=answer.get('active'),
+        id_question=answer.get('id_question'),
+        id_survey=answer.get('id_survey'),
+        id_survey_section=answer.get('id_survey_section'),
+        create_by=answer.get('user_id')
         )
-        session.add(ans)
+        models.session.add(ans)
+    models.session.commit()
 
+    # Comparamos el id_risk_profile con el resultado obtenido
     if result is None:
         id_risk_profile = None;
         msg = 'Usted por el momento no puede acceder a nuestros productos.';
     elif result < -1:
         id_risk_profile = 1
-        msg = 'Usted tiene un perfil tipo: Adverso, un asesor se comunicará con usted próximamente.'
+        msg = 'Usted tiene un perfil tipo: <b>Adverso</b></br></br>Un asesor se comunicará con usted próximamente.'
     elif result <= 0:
         id_risk_profile = 2
-        msg = 'Usted tiene un perfil tipo: Moderado Adverso, un asesor se comunicará con usted próximamente.'
+        msg = 'Usted tiene un perfil tipo: <b>Moderado Adverso</b></br></br>Un asesor se comunicará con usted próximamente.'
     elif result > 0.75:
         id_risk_profile = 4
-        msg = 'Usted tiene un perfil tipo: Agresivo, un asesor se comunicará con usted próximamente.'
+        msg = 'Usted tiene un perfil tipo: <b>Agresivo</b></br></br>Un asesor se comunicará con usted próximamente.'
     else:
         id_risk_profile = 3;
-        msg = 'Usted tiene un perfil tipo: Moderado Agresivo, un asesor se comunicará con usted próximamente.'
-
-    user = session.query(User).filter(User.id==id_user).first()
-    user.age = id_age
+        msg = 'Usted tiene un perfil tipo: <b>Moderado Agresivo</b></br></br>Un asesor se comunicará con usted próximamente.'
 
     evaluation = Evaluation(
-        id_risk_profile = id_risk_profile,
-        id_survey = id_survey,
-        id_user = id_user,
-        result = result
+        id_risk_profile=id_risk_profile,
+        id_survey=id_survey,
+        id_user=id_user,
+        result=result
     )
+    models.session.add(evaluation)
+    models.session.commit()
+
+    print(evaluation)
+
+    user = models.session.query(User).filter(User.id==id_user).first()
+    user.age = id_age
+    models.session.add(user)
+    models.session.commit()
+
     print(result)
     print(user.age)
-    session.add(evaluation)
 
-    session.commit()
+    print(models.session.query(Evaluation).all())
     return jsonify({'status': 'ok', 'msg': msg})
